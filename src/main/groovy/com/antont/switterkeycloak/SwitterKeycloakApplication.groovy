e22f2846a606ced488387c392d1694438d87ced4
Anton Turchenko <antont@default-value.com>
2024-04-25 19:50:18 +0300

    - initial commit

---
+++ src/main/groovy/com/antont/switterkeycloak/SwitterKeycloakApplication.groovy
@@ -0,0 +1,13 @@
+package com.antont.switterkeycloak
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SwitterKeycloakApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SwitterKeycloakApplication, args)
+    }
+
+}
---
+++ src/test/groovy/com/antont/switterkeycloak/SwitterKeycloakApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.antont.switterkeycloak
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SwitterKeycloakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
