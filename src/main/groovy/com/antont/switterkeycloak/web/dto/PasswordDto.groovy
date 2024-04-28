d6aaa3f0a376908464897f326b582e32742c2e7c
Anton Turchenko <antont@default-value.com>
2024-04-28 17:11:20 +0300

    - removed unnecessary lombok dependency

--- src/main/groovy/com/antont/switterkeycloak/web/dto/PasswordDto.groovy
+++ src/main/groovy/com/antont/switterkeycloak/web/dto/PasswordDto.groovy
@@ -4,9 +4,7 @@ import com.antont.switterkeycloak.validation.PasswordMatches
 import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
-import lombok.NoArgsConstructor
 
-@NoArgsConstructor
 @PasswordMatches
 class PasswordDto {
     @NotNull
